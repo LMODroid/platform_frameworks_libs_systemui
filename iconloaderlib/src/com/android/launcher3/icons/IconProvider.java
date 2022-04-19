d6918b28269faf50a1d06e8641e4aa3303107463
DennySPb <dennyspb@gmail.com>
2022-04-19 10:57:59 +0300

    Add support for overriding of IconProvider
    Utils for resource based overrides were taken from launcher
    
    Change-Id: I38215decdfe7235ed9a64f78f2fc90eac6f9c654
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- iconloaderlib/src/com/android/launcher3/icons/IconProvider.java
+++ iconloaderlib/src/com/android/launcher3/icons/IconProvider.java
@@ -16,6 +16,8 @@
 
 package com.android.launcher3.icons;
 
+import static com.android.launcher3.util.override.MainThreadInitializedObject.forOverride;
+
 import static android.content.Intent.ACTION_DATE_CHANGED;
 import static android.content.Intent.ACTION_TIMEZONE_CHANGED;
 import static android.content.Intent.ACTION_TIME_CHANGED;
@@ -53,13 +55,16 @@ import androidx.core.os.BuildCompat;
 
 import com.android.launcher3.util.SafeCloseable;
 
+import com.android.launcher3.util.override.MainThreadInitializedObject;
+import com.android.launcher3.util.override.ResourceBasedOverride;
+
 import java.util.Calendar;
 import java.util.Objects;
 
 /**
  * Class to handle icon loading from different packages
  */
-public class IconProvider {
+public class IconProvider implements ResourceBasedOverride {
 
     private static final String TAG = "IconProvider";
     private static final boolean DEBUG = false;
@@ -75,6 +80,9 @@ public class IconProvider {
 
     @NonNull
     protected String mSystemState = "";
+
+    public static MainThreadInitializedObject<IconProvider> INSTANCE =
+            forOverride(IconProvider.class, R.string.icon_provider_class);
 
     public IconProvider(Context context) {
         mContext = context;
