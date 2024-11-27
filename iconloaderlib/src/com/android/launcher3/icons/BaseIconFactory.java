20189e98291fb5cb77b855fcfdd98a94087d1888
Pranav Vashi <neobuddy89@gmail.com>
2024-11-28 00:44:30 +0530

    iconloaderlib: Do not wrap themed icons
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/570
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: If4236ae1b76d73f786b4e9d55ff2930ef2aecc97
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- iconloaderlib/src/com/android/launcher3/icons/BaseIconFactory.java
+++ iconloaderlib/src/com/android/launcher3/icons/BaseIconFactory.java
@@ -231,18 +231,18 @@ public class BaseIconFactory implements AutoCloseable {
     public BitmapInfo createBadgedIconBitmap(@NonNull Drawable icon,
             @Nullable IconOptions options) {
         float[] scale = new float[1];
-        AdaptiveIconDrawable adaptiveIcon = normalizeAndWrapToAdaptiveIcon(icon, null, scale);
-        Bitmap bitmap = createIconBitmap(adaptiveIcon, scale[0],
+        Drawable tempIcon = normalizeAndWrapToAdaptiveIcon(icon, null, scale);
+        Bitmap bitmap = createIconBitmap(tempIcon, scale[0],
                 options == null ? MODE_WITH_SHADOW : options.mGenerationMode);
 
         int color = (options != null && options.mExtractedColor != null)
                 ? options.mExtractedColor : mColorExtractor.findDominantColorByHue(bitmap);
         BitmapInfo info = BitmapInfo.of(bitmap, color);
 
-        if (adaptiveIcon instanceof BitmapInfo.Extender extender) {
+        if (tempIcon instanceof BitmapInfo.Extender extender) {
             info = extender.getExtendedInfo(bitmap, color, this, scale[0]);
-        } else if (mMonoIconEnabled) {
-            Drawable mono = getMonochromeDrawable(adaptiveIcon);
+        } else if (mMonoIconEnabled && tempIcon instanceof AdaptiveIconDrawable) {
+            Drawable mono = getMonochromeDrawable((AdaptiveIconDrawable) tempIcon);
             if (mono != null) {
                 info.setMonoIcon(createIconBitmap(mono, scale[0], MODE_ALPHA), this);
             }
@@ -257,7 +257,7 @@ public class BaseIconFactory implements AutoCloseable {
         info = info.withFlags(getBitmapFlagOp(options));
         return info;
     }
-    
+
     public void setMonoIconEnabled(boolean enabled) {
         mMonoIconEnabled = enabled;
     }
@@ -340,18 +340,21 @@ public class BaseIconFactory implements AutoCloseable {
     }
 
     @Nullable
-    protected AdaptiveIconDrawable normalizeAndWrapToAdaptiveIcon(@Nullable Drawable icon,
+    protected Drawable normalizeAndWrapToAdaptiveIcon(@Nullable Drawable icon,
             @Nullable final RectF outIconBounds, @NonNull final float[] outScale) {
         if (icon == null) {
             return null;
         }
 
-        AdaptiveIconDrawable adaptiveIcon;
         float scale;
-        adaptiveIcon = wrapToAdaptiveIcon(icon, outIconBounds);
-        scale = getNormalizer().getScale(adaptiveIcon, outIconBounds, null, null);
+        if ((icon.getChangingConfigurations() & CONFIG_HINT_NO_WRAP) == 0) {
+            AdaptiveIconDrawable adaptiveIcon;
+            adaptiveIcon = wrapToAdaptiveIcon(icon, outIconBounds);
+            icon = adaptiveIcon;
+        }
+        scale = getNormalizer().getScale(icon, outIconBounds, null, null);
         outScale[0] = scale;
-        return adaptiveIcon;
+        return icon;
     }
 
     /**
@@ -386,7 +389,7 @@ public class BaseIconFactory implements AutoCloseable {
             dr.setBounds(0, 0, 1, 1);
             boolean[] outShape = new boolean[1];
             float scale = getNormalizer().getScale(icon, outIconBounds, dr.getIconMask(), outShape);
-            if (!outShape[0] && (icon.getChangingConfigurations() & CONFIG_HINT_NO_WRAP) == 0) {
+            if (!outShape[0]) {
                 foreground.setDrawable(createScaledDrawable(icon, scale * LEGACY_ICON_SCALE));
             } else {
                 foreground.setDrawable(createScaledDrawable(icon, 1 - getExtraInsetFraction()));
