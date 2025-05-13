d5f836b7c7ecec00d5dd53bce370b14497c1aea1
Naresh Kumar Podishetty (xWF) <podishettyk@google.com>
2025-05-12 19:45:32 -0700

    Revert "[MM] Nudge immediately when we reach a breakpoint"
    This reverts commit 386c486c39a776f48619a1eb3b10a87b7c74ad11.
    
    Reason for revert: <Droid monitor: Likely culprit for b/417266157 - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.>
    
    Bug: 417266157
    Change-Id: I500ac7792e861eefb32829afd5ef010321c57b88
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- mechanics/src/com/android/mechanics/impl/Computations.kt
+++ mechanics/src/com/android/mechanics/impl/Computations.kt
@@ -481,6 +481,13 @@ internal abstract class Computations : CurrentFrameInput, LastFrameState, Static
                                 guaranteeState.updatedSpringParameters(lastBreakpoint)
                         }
 
+                        springState =
+                            springState.calculateUpdatedState(
+                                nextBreakpointCrossTime - lastAnimationTime,
+                                springParameters,
+                            )
+                        lastAnimationTime = nextBreakpointCrossTime
+
                         val mappingBefore = mappings[segmentIndex]
                         val beforeBreakpoint = mappingBefore.map(nextBreakpoint.position)
                         val mappingAfter = mappings[segmentIndex + directionOffset]
@@ -498,18 +505,7 @@ internal abstract class Computations : CurrentFrameInput, LastFrameState, Static
                                     "  after: $afterBreakpoint (mapping: $mappingAfter)",
                             )
                         }
-
-                        if (!hasJumped && delta != 0f) {
-                            hasJumped = true
-                            springState = springState.nudge(velocityDelta = directMappedVelocity)
-                        }
-
-                        springState =
-                            springState.calculateUpdatedState(
-                                nextBreakpointCrossTime - lastAnimationTime,
-                                springParameters,
-                            )
-                        lastAnimationTime = nextBreakpointCrossTime
+                        hasJumped = hasJumped || delta != 0f
 
                         if (deltaIsFinite) {
                             springState = springState.nudge(displacementDelta = -delta)
@@ -532,6 +528,10 @@ internal abstract class Computations : CurrentFrameInput, LastFrameState, Static
 
                                 is Guarantee.None -> GuaranteeState.Inactive
                             }
+                    }
+
+                    if (hasJumped) {
+                        springState = springState.nudge(velocityDelta = directMappedVelocity)
                     }
 
                     val tightened = guarantee.updatedSpringParameters(segment.entryBreakpoint)
